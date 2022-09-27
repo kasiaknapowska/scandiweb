e3edcd94a6886d45b987f9895759aa2e2c6e1c2f
kasiaknapowska <k.knapowska@gmail.com>
2022-09-27 13:32:08 +0200

    cart image slider, cart styles

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -4,11 +4,15 @@ import { connect } from "react-redux";
 import { substractFromCart, addToCart } from "../../redux/cartSlice";
 import { addCount, substractCount } from "../../redux/counterSlice";
 import Attributes from "../Attributes";
+import SliderArrow from "../SliderArrow/SliderArrow";
 import "./_CartItem.scss";
 
 class CartItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentImageIndex: 0,
+    };
   }
 
   addItem(item) {
@@ -18,6 +22,19 @@ class CartItem extends Component {
   substractItem(item) {
     this.props.substractFromCart(item);
     this.props.substractCount();
+  }
+
+  goToPrevious(gallery) {
+    const isFirstSlide = this.state.currentImageIndex === 0;
+    const newIndex = isFirstSlide
+      ? gallery.length - 1
+      : this.state.currentImageIndex - 1;
+    this.setState({ currentImageIndex: newIndex });
+  }
+  goToNext(gallery) {
+    const isLastSlide = this.state.currentImageIndex === gallery.length - 1;
+    const newIndex = isLastSlide ? 0 : this.state.currentImageIndex + 1;
+    this.setState({ currentImageIndex: newIndex });
   }
 
   render() {
@@ -72,10 +89,36 @@ class CartItem extends Component {
             -
           </div>
         </div>
-        <div
-          className="item_img"
-          style={{ backgroundImage: `url(${this.props.item.gallery[0]})` }}
-        ></div>
+        {this.props.type === "minicart" && (
+          <div
+            className="item_img"
+            style={{ backgroundImage: `url(${this.props.item.gallery[0]})` }}
+          ></div>
+        )}
+        {this.props.type === "cart" && (
+          <div
+            className="item_img"
+            style={{
+              backgroundImage: `url(${
+                this.props.item.gallery[this.state.currentImageIndex]
+              })`,
+            }}
+          >
+            {this.props.item.gallery.length > 1 && (
+              <div className="arrows_container">
+                {" "}
+                <SliderArrow
+                  direction="previous"
+                  onClick={() => this.goToNext(this.props.item.gallery)}
+                />
+                <SliderArrow
+                  direction="next"
+                  onClick={() => this.goToPrevious(this.props.item.gallery)}
+                />
+              </div>
+            )}
+          </div>
+        )}
       </div>
     );
   }
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -4,23 +4,62 @@ import CartItem from "../../components/CartItem";
 import "./_CartPage.scss";
 
 class CartPage extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+
 
   render() {
-    console.log(this.props.count);
-    console.log(this.props.cart);
+    // console.log(this.props.count);
+    // console.log(this.props.cart);
+    const price = this.props.totalPrice.filter(
+      (price) => price.currency.symbol === this.props.currency
+    )[0].amount;
     return (
       <main className="container cart_page">
         <h1>Cart</h1>
+        <div className="line"></div>
         {this.props.count > 0 && (
           <>
             {this.props.cart.map((item, index) => (
-              <CartItem key={item.id + index} item={item} type="cart"/>
+              <>
+                <CartItem
+                  key={item.id + index}
+                  item={item}
+                  type="cart"
+                  // currentImageIndex={this.state.currentImageIndex}
+                  // gotToNext={this.goToNext}
+                  // gotToPrevious={this.goToPrevious}
+                />
+                <div className="line" key={"line" + index}></div>
+              </>
             ))}
           </>
         )}
+        <table>
+          <tr>
+            <th>Tax 21%:</th>
+            <td>
+              {this.props.currency}
+              &nbsp;
+              {(price * 0.21).toFixed(2)}
+            </td>
+          </tr>
+          <tr>
+            <th>Quantity:</th>
+            <td>{this.props.count}</td>
+          </tr>
+          <tr>
+            <th>Total:</th>
+            <td>
+              {this.props.currency}
+              &nbsp;
+              {this.props.totalPrice
+                .filter(
+                  (price) => price.currency.symbol === this.props.currency
+                )[0]
+                .amount.toFixed(2)}
+            </td>
+          </tr>
+        </table>
+        <button className="btn_primary btn_order">Order</button>
       </main>
     );
   }
